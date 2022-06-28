3920d51f505e3247a303f64ba5b7288999368222
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-06-28 15:10:04 -0300

    adjusting global style and creating theme

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from '../styles/global';
+import { theme } from '../styles/theme';
 
 interface AppProviderProps {
   children: React.ReactNode;
@@ -10,8 +12,10 @@ interface AppProviderProps {
 const AppProvider: FC<AppProviderProps> = ({ children }) => {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      {children}
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    black: '#000',
+  },
+
+  fonts: {
+    roboto: 'Roboto, sans-serif',
+  },
+};
