6f24f3159e953cefeb1e730f3907e032e9d21056
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2023-03-29 10:14:24 -0300

    verifying displayed items

--- src/pages/ManualImplementation/Virtualizer.tsx
+++ src/pages/ManualImplementation/Virtualizer.tsx
@@ -1,4 +1,6 @@
-import { ItemContainer, VirtualizerContainer } from './styles';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import { Container, ItemContainer, VirtualizerContainer } from './styles';
 
 interface VirtualizerProps {
   containerHeight: number;
@@ -17,6 +19,13 @@ function Virtualizer({
   gap = 0,
   children,
 }: VirtualizerProps): JSX.Element {
+  const [displayingItems, setDisplayingItems] = useState([]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const containerRef = useRef<any>();
+
+  const items = Array.from(new Array(itemCount));
+
   const calculateItemTop = (index: number): number => {
     if (index === 0) return 0;
 
@@ -27,17 +36,62 @@ function Virtualizer({
     return previousItemsHeight;
   };
 
-  const items = Array.from(new Array(itemCount));
+  const itemTopPositions: Record<number, number> = items.reduce((acc, item, index) => {
+    const itemTop = calculateItemTop(index);
+
+    const positions = {
+      ...acc,
+      [index]: itemTop,
+    };
+
+    return positions;
+  }, {});
+
+  const handleScroll = useCallback(() => {
+    const { scrollTop } = containerRef.current;
+    const contentRenderedUntil = scrollTop + containerHeight;
+
+    const displaying = items.reduce((acc, _, index) => {
+      const itemTop = itemTopPositions[index];
+
+      if (itemTop + itemHeight >= scrollTop && itemTop <= contentRenderedUntil) {
+        return [...acc, index];
+      }
+
+      return acc;
+    }, []);
+
+    setDisplayingItems(displaying);
+  }, [containerHeight, itemHeight, itemTopPositions, items]);
+
+  useEffect(() => {
+    handleScroll();
+  }, [handleScroll]);
 
   return (
-    <VirtualizerContainer $height={containerHeight} $width={containerWidth}>
-      {items.map((_, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <ItemContainer key={`item-${index}`} $top={calculateItemTop(index)} $height={itemHeight}>
-          {children(index)}
-        </ItemContainer>
-      ))}
-    </VirtualizerContainer>
+    <Container>
+      <VirtualizerContainer
+        ref={containerRef}
+        $height={containerHeight}
+        $width={containerWidth}
+        onScroll={handleScroll}
+      >
+        {items.map((_, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <ItemContainer key={`item-${index}`} $top={itemTopPositions[index]} $height={itemHeight}>
+            {children(index)}
+          </ItemContainer>
+        ))}
+      </VirtualizerContainer>
+
+      {!!displayingItems.length && (
+        <div>
+          <strong>Items em tela: </strong>
+
+          <span>{displayingItems.map((itemIndex) => itemIndex + 1).join(', ')}</span>
+        </div>
+      )}
+    </Container>
   );
 }
 
--- src/pages/ManualImplementation/styles.ts
+++ src/pages/ManualImplementation/styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  padding-top: 100px;
+`;
+
 interface VirtualizerContainerProps {
   $height: number;
   $width: number;
